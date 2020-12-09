0abc8d23c1b4d355b405277d90afa721e762e1de
ellendusrud <31755208+ellendusrud@users.noreply.github.com>
2020-12-09 14:43:37 +0900

    Update app.js

--- app.js
+++ app.js
@@ -1,10 +1,17 @@
-let express = require("express");
-let cors = require("cors");
-let app = express();
+var express = require('express');
+var cors = require('cors');
+var app = express();
 
-app.get('/', (req,res) => {
-  res.header("Access-Control-Allow-Origin", "*");
-}
+// CORS 설정
+app.use(cors());
+
+app.get('/products/:id', function (req, res, next) {
+  res.json({msg: 'This is CORS-enabled for all origins!'})
+});
+
+app.listen(80, function () {
+  console.log('CORS-enabled web server listening on port 80')
+});
 
 // let cors_origin = [`http://apis.data.go.kr`];
 //
